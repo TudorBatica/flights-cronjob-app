802c78bc37d822bcfbbd7bb26684b923428a2d63
TudorBatica <tudor.batica@gmail.com>
2024-05-27 09:54:56 +0300

    add startup logs in flights-web

--- flights-web/src/main.rs
+++ flights-web/src/main.rs
@@ -6,7 +6,12 @@ use flights_web::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    println!("Starting flights-web...");
     let configuration = configuration::get_configuration();
+    println!(
+        "Trying to connect to database at {}",
+        configuration.database_url
+    );
     let pool = PgPool::connect(&configuration.database_url).await.unwrap();
     let addr = format!("0.0.0.0:{}", configuration.app_port);
     let listener = TcpListener::bind(addr).unwrap();
